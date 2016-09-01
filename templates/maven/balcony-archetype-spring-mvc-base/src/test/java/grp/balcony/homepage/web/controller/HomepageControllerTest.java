15ef715bc34f9c9190367f6f094bbd4e6258c8e6
symore <symore@ephu-linuxbox>
2016-09-01 16:18:12 +0200

    Initial version of base spring mvc archetype added

---
+++ templates/maven/balcony-archetype-spring-mvc-base/src/test/java/grp/balcony/homepage/web/controller/HomepageControllerTest.java
@@ -0,0 +1,23 @@
+package grp.balcony.homepage.web.controller;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomepageControllerTest {
+    private HomepageController underTest;
+
+    @BeforeMethod
+    public void setUp() {
+        underTest = new HomepageController();
+    }
+
+    @Test
+    public void testHomepageShouldReturnHome() {
+        // GIVEN in setup
+        // WHEN
+        String result = underTest.homepage();
+        // THEN
+        Assert.assertEquals(result, "home");
+    }
+}
